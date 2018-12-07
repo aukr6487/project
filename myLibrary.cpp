2cbf5a21ced3691e35f02f1eec2a24e6f1a91d2a
aukr6487 <45699364+aukr6487@users.noreply.github.com>
2018-12-07 13:42:03 -0700

    Create myLibrary.cpp

---
+++ myLibrary.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+using namespace std;
+
+// Binary Tree Node
+struct Node {
+    string letter;
+    Node *left, *right;
+};
+
+
+/*void deleteMovieTree(MovieNode* node){
+  if(node == NULL){
+    return;
+  }
+  deleteMovieTree(node->leftChild);
+  delete node;
+  deleteMovieTree(node->rightChild);
+return;
+}*/
+
+/*MovieNode* MovieTree::search(string title){
+  if(root==NULL){
+    return NULL;
+  }
+  MovieNode *current = root;
+  while(current!=NULL){
+    if(current->title == title){
+      return current;
+    }
+    if(title < current->title){
+      current = current->leftChild;
+    } else {
+      current = current->rightChild;
+    }
+  }
+  return current;
+}*/
+
+/*MovieNode* printMovie(MovieNode *node){
+  if(node!=NULL){
+    printMovie(node->leftChild);
+    cout<<"Movie: "<< node->title <<" "<<node->quantity<<endl;
+    printMovie(node->rightChild);
+  }
+  return node;
+}*/
+
+void addArtist(string name){
+  Node *newArtist = newNode(name);
+  MovieNode *current = root;
+  while(root!=NULL){
+    if( == M){
+
+    }
+  }
+  MovieNode *newMovie = new MovieNode(ranking, title, year, quantity);
+  if(root == NULL){
+    root = newMovie;
+    return;
+  }
+  MovieNode *current = root;
+  while(root!=NULL){
+    if(current->title == title){
+        current->quantity++;
+        return;
+    }
+    else if(title < current->title){
+      if(current->leftChild == NULL){
+        current->leftChild = newMovie;
+        newMovie->parent = current;
+        return;
+      }
+      current = current->leftChild;
+    } else if(title > current->title){
+      if(current->rightChild == NULL){
+        current->rightChild = newMovie;
+        newMovie->parent = current;
+        return;
+      }
+      current = current->leftChild;
+    }
+  }
+}
+
+Node* newNode(string letter){
+    Node* node = new Node;
+    node->letter = letter;
+    node->left = node->right = NULL;
+    return (node);
+}
+
+int main(){
+  Node* root = newNode(M);
+    // Binary Tree is:
+    //       M
+    //      /  \
+    //
+    //    / \  / \
+    //   4   5 6  7
+    //          \
+    //           8
+    /*Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
+    root->right->left->right = newNode(8);*/
+
+    return 0;
+}
